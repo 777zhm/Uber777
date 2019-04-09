f2b18ec6f8b253048173fea16062200318fce1ac
Yuriy Zhmurko <777zhm@gmail.com>
2019-04-09 18:01:41 +0300

    The end

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
 	
-	<title><?php the_title(); ?></title>
+	<title><?php echo "Uber-Partners | "; the_title(); ?></title>
 	<?php wp_head(); ?>
 </head>
 
--- template-mainpage.php
+++ template-mainpage.php
@@ -4,30 +4,37 @@
 <section class="heroscreen" style="background-image: url(<?php assets_url('img/heroscreen_bg.jpg') ?>);">
 	<div class="container">
 		<div class="row">
-			<div class="col-sm-8">
-
+			<div class="col">
+				<div class="h1_box">
+					<h1>Uber-Partners - Ваш надежный партнер</h1>
+				</div>
 			</div>
-			<div class="col-sm-4">
+			<div class="col">
 				<form class="contact_form">
 					<div class="form_header">
 						<h4>Подключиться к Uber</h4>
 					</div>
-					
-					<input type="text" class="form-control" placeholder="ФИО" name="useredrpou" required>
-					<input type="tel" class="form-control" placeholder="Телефон" name="usertelephone" required>
-					<input type="email" class="form-control" placeholder="E-mail" name="useremail" required>
-					<label for="driverlicense">Права
-						<input type="file" id="driverlicense" name="driverlicense" accept="image/png, image/jpeg">
-					</label>
-					<label for="techpassport">Техпаспорт
-						<input type="file" id="techpassport" name="techpassport" accept="image/png, image/jpeg">
-					</label>
-					<label for="insurance">Страховка
-						<input type="file" id="insurance" name="insurance" accept="image/png, image/jpeg">
-					</label>
-					<div class="form_footer">
-						<button type="submit" class="btn_filled">Отправить заявку</button>
+					<div class="form_body">
+						<input type="text" class="form-control" placeholder="ФИО" name="useredrpou" required>
+						<input type="tel" class="form-control" placeholder="Телефон" name="usertelephone" required>
+						<input type="email" class="form-control" placeholder="E-mail" name="useremail" required>
+						<label for="driverlicense">Права
+							<img src="<?php assets_url('img/upload.png') ?>" />
+							<input type="file" id="driverlicense"  name="driverlicense" />
+						</label>
+						
+						<label for="techpassport">Техпаспорт
+							<img src="<?php assets_url('img/upload.png') ?>" />
+							<input type="file" id="techpassport"  name="techpassport" />
+						</label>
+						
+						<label for="insurance">Страховка
+							<img src="<?php assets_url('img/upload.png') ?>" />
+							<input type="file" id="insurance"  name="insurance" />
+						</label>				
+
 					</div>
+						<button type="submit" class="form_footer btn_filled">Отправить заявку</button>
 				</form>
 			</div>
 		</div>
@@ -35,33 +42,52 @@
 </section>
 <section class="aboutus">
 	<div class="container">
-		<div class="row">
+		<div class="row justify-content-center">
 			<div class="col-sm-12">
 				<h2 class="section_heading">Про Uber-Partners</h2>
 			</div>
-			<div class="col-sm-3">
-				<h3 class="block_heading">4 городa</h3>
-				<p>Наши офисы представлены в Киеве, Харькове, Львове и Одессе</p>
+			<div class="col-md-3 col-sm-6">
+				<div class="aboutus_item">
+					<div class="ribbon"><span>о нас</span></div>
+					<img class="aboutus_item_icon" src="<?php assets_url('img/city.png') ?>" alt="city_icon">
+					<h3 class="block_heading">4 городa</h3>
+					<p>Наши офисы представлены в Киеве, Харькове, Львове и Одессе</p>
+				</div>
 			</div>
-			<div class="col-sm-3">
-				<h3 class="block_heading">100 водителей</h3>
-				<p>Более 100 водителей выбрали нас</p>
+			<div class="col-md-3 col-sm-6">
+				<div class="aboutus_item">
+					<div class="ribbon"><span>о нас</span></div>
+					<img class="aboutus_item_icon" src="<?php assets_url('img/driver.png') ?>" alt="driver_icon">
+					<h3 class="block_heading">100 водителей</h3>
+					<p>Более 100 водителей выбрали нас</p>
+				</div>
 			</div>
-			<div class="col-sm-3">
-				<h3 class="block_heading">500 000 км</h3>
-				<p>500 тыс км совокупно проехали наши водители за 2018 год </p>
+			<div class="col-md-3 col-sm-6">
+				<div class="aboutus_item">
+					<div class="ribbon"><span>о нас</span></div>
+					<img class="aboutus_item_icon" src="<?php assets_url('img/waypoint.png') ?>" alt="waypoint_icon">
+					<h3 class="block_heading">500 000 км</h3>
+					<p>500 тыс км совокупно проехали наши водители за 2018 год </p>
+				</div>
 			</div>
-			<div class="col-sm-3">
-				<h3 class="block_heading">22 000 грн</h3>
-				<p>Средний доход активного водителя Uber-Partners</p>
+			<div class="col-md-3 col-sm-6">
+				<div class="aboutus_item">
+					<div class="ribbon"><span>о нас</span></div>
+					<img class="aboutus_item_icon" src="<?php assets_url('img/money.png') ?>" alt="money_icon">
+					<h3 class="block_heading">22 000 грн</h3>
+					<p>Средний доход активного водителя Uber-Partners</p>
+				</div>
 			</div>
 		</div>
-		<div class="row justify-content-center">
+		<div class="row justify-content-center advantages_row">
 			<div class="advantages_item">
-				<p>Официальный партнер Uber в Украине</p>
+				<p>Официальный партнер Uber</p>
 			</div>
 			<div class="advantages_item">
 				<p>Оптимальные ставки комиссии</p>
+			</div>
+			<div class="advantages_item">
+				<p>Своевременные выплаты</p>
 			</div>
 			<div class="advantages_item">
 				<p>Удобный вывод заработка</p>
@@ -70,21 +96,18 @@
 				<p>Ускоренная регистрация</p>
 			</div>
 			<div class="advantages_item">
-				<p>Своевременные выплаты</p>
-			</div>
-			<div class="advantages_item">
 				<p>Обучение и консультации</p>
-			</div>
+			</div>			
 		</div>
 	</div>
 </section>
 <section class="joinus">
 	<div class="container">
-		<div class="row">
+		<div class="row justify-content-center">
 			<div class="col-sm-12">
 				<h2 class="section_heading">Присоединяйтесь к команде Uber-Partners</h2>
 			</div>
-			<div class="col-sm-4">
+			<div class="col-md-4 col-sm-6 joinus_item">
 				<img src="<?php assets_url('img/join_1.jpg') ?>" class="img img-fluid" alt="join_1">
 				<h3 class="block_heading">На своем авто в Uber</h3>
 				<p>Станьте водителем Uber на своем авто. Зарабатывайте в режиме полной занятости или в свободное время с функцией “по маршруту”.</p>
@@ -92,7 +115,7 @@
 					<button class="btn_base">Зарегистрироваться</button>
 				</div>
 			</div>
-			<div class="col-sm-4">
+			<div class="col-md-4 col-sm-6 joinus_item">
 				<img src="<?php assets_url('img/join_2.jpg') ?>" class="img img-fluid" alt="join_2">
 				<h3 class="block_heading">На авто компании в Uber</h3>
 				<p>Станьте водителем Uber на авто компании. Работайте на новых и исправных авто в категориях UberX, UberSelect, UberBlack. <br><br></p>
@@ -100,7 +123,7 @@
 					<button class="btn_base">Зарегистрироваться</button>
 				</div>
 			</div>
-			<div class="col-sm-4">
+			<div class="col-md-4 col-sm-6 joinus_item">
 				<img src="<?php assets_url('img/join_3.jpg') ?>" class="img img-fluid" alt="join_3">
 				<h3 class="block_heading">Курьер в UberEats</h3>
 				<p>Станьте курьером в сервисе UberEats. Доставляй еду на своем авто, велосипеде или скутере. Вам не нужны права и опыт работы. <br><br></p>
